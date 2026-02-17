3e7b37bfd17c1b36441435efd684f48b37598a3d
Nour <naoude@gmail.com>
2026-02-17 23:14:13 +0200

    Add migrations and repo

--- cmd/app.go
+++ cmd/app.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5"
+)
+
+
+type App struct {
+	config AppConfig
+	conn *pgx.Conn
+}
+
+type AppConfig struct {
+	port string
+	dsn string
+}
+
+func (this *App) run(h http.Handler) error {
+	server := http.Server{
+		Addr: this.config.port,
+		Handler: h,
+	}
+	log.Printf("Server has started at address %s", this.config.port)
+	return server.ListenAndServe()
+}
+
+func (this *App) mount() http.Handler {
+	router := chi.NewRouter()
+
+	// Middleware
+	// Routes
+	router.Get("/health", func(w http.ResponseWriter, r *http.Request ) {
+		log.Printf("Health check!")
+		w.Write([]byte("All good!"))
+	})
+	return router
+}
--- cmd/main.go
+++ cmd/main.go
@@ -2,34 +2,23 @@ package main
 
 import (
 	"context"
-	"log/slog"
-	"os"
+	"log"
 
 	"github.com/jackc/pgx/v5"
 )
-
-type App struct {
-	config    AppConfig
-	conn *pgx.Conn
-}
-
-type AppConfig struct {
-	port  string
-	dbString string
-}
 
 func main() {
 	ctx := context.Background()
 	config := AppConfig{
 		port: ":8080",
-		dbString: "host=localhost port=5434 user=naoude password=6%71hiu& dbname=formulator sslmode=disable",
+		dsn: "host=localhost port=5434 user=naoude password=6%71hiu& dbname=formulator sslmode=disable",
 	}
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	slog.SetDefault(logger)
-	conn, err := pgx.Connect(ctx, config.dbString)
+	// slogger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	// slog.SetDefault(slogger)
+	conn, err := pgx.Connect(ctx, config.dsn)
 	if err != nil {
-		logger.Error("Unable to connect to db", err)
-		panic(err)
+		// slogger.Error("Unable to connect to db", "error", err)
+		log.Panicf("Error connecting to database: %s", err)
 	}
 	defer conn.Close(ctx)
 	app := App{
@@ -37,4 +26,18 @@ func main() {
 		conn: conn,
 	}
 	// run application
+	if err := app.run(app.mount()); err != nil {
+		log.Fatalf("Error starting server: %s", err)
+	}
+	
+	// Mini app
+	// router := chi.NewRouter()
+	// router.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("Simple health check, all good!"))
+	// })
+	// _, err := pgx.Connect(context.Background(), "host=localhost port=5434 user=naoude password=6%71hiu& dbname=formulator sslmode=disable")
+	// if err != nil {
+	// 	log.Fatal("Error connecting to db")
+	// }
+	// http.ListenAndServe(":8080", router)
 }
